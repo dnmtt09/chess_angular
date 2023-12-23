2f515db4ca3b1a2f65cbf033a4c9a198aa48acc8
Daniele Matta <daniele.matta@aicof.it>
2023-12-23 20:07:08 +0100

    initializing build chessboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ChessboardComponent} from './chessboard/chessboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChessboardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/chessboard/chessboard.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-chessboard',
+    templateUrl: './chessboard.component.html',
+    styleUrls: ['./chessboard.component.scss']
+})
+export class ChessboardComponent {
+}
